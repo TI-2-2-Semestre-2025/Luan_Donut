5c6b26fa382cf111aec20127018246b373b2e4f3
Bernardo O. Horta <bernardo.o.horta@gmail.com>
2025-10-14 12:03:56 -0300

    CharacterController Parte 1

--- Assets/Scripts/Player/Player_Movement.cs
+++ Assets/Scripts/Player/Player_Movement.cs
@@ -1,57 +1,38 @@
-using System;
 using System.Collections;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
 
 public class Player_Movement : MonoBehaviour
 {
     public int lane=0;
-    public float offset;
+    public float gravityForce;
     private bool roll = false;
     
-    private Rigidbody _rigidbody;
-    private CapsuleCollider _collider;
+    private CharacterController _characterController;
     private Player_EntityStats _entityStats;
     public GameObject playerModel;
 
     private void Start()
     {
         _entityStats = GetComponent<Player_EntityStats>();
-        _rigidbody = GetComponent<Rigidbody>();
-        _collider = GetComponent<CapsuleCollider>();
+        _characterController = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
         Z_Movement();
-
+        Gravity();
         Level_Manager.Instance.playerDistance = transform.position.z;
+    }
+
+    private void Gravity()
+    {
+        _characterController.Move(gravityForce * Time.deltaTime * -transform.up);
     }
 
     private void Z_Movement()
     {
-        _rigidbody.AddForce(_entityStats.speed * 100 * Time.deltaTime * transform.forward);
+        _characterController.Move(_entityStats.speed * Time.deltaTime * transform.forward);
         _entityStats.speed += _entityStats.speedGain * Time.deltaTime;
-    }
-    
-    
-    public void Hit()
-    {
-        _entityStats.speed = (_entityStats.speed+_entityStats.defSpeed)/1.5f;
-        StartCoroutine(I_FlashPlayer());
-    }
-
-    private IEnumerator I_FlashPlayer()
-    {
-        Vector3 dposition = playerModel.transform.position;
-        
-        for (int i = 0; i < 5; i++)
-        {
-            playerModel.transform.Translate(Vector3.up * 1000);
-            yield return new WaitForSeconds(0.2f);
-            playerModel.transform.Translate(Vector3.up * -1000);
-            yield return new WaitForSeconds(0.2f);
-        }
     }
 
     // 1 => Right / -1 => Left
@@ -60,39 +41,49 @@ public class Player_Movement : MonoBehaviour
         lane += direction;
         if (lane is <= 1 and >= -1)
         {
-            StartCoroutine(I_ChangeLane(direction));
+            float laneOffset = Game_Manager.Instance.laneOffset;
+            _characterController.Move(direction * laneOffset * transform.right);
         }else lane -= direction;
     }
-
+    
+    /*
     IEnumerator I_ChangeLane(int direction)
     {
-        float count = 0;
-        while (count < _entityStats.changeLaneSeconds)
-        {
-            float offset = Game_Manager.Instance.laneOffset;
-            transform.Translate(Time.deltaTime/_entityStats.changeLaneSeconds * offset * direction * transform.right);
-            count += Time.deltaTime;
+        float laneOffset = Game_Manager.Instance.laneOffset;
+        Vector3 startPosition = new Vector3(transform.position.x, 0, 0);
+        Vector3 endPosition = startPosition + (direction * laneOffset * transform.right);
 
+        for (float i = 0; i < _entityStats.changeLaneSeconds; i += Time.deltaTime)
+        {
+            float t = i / _entityStats.changeLaneSeconds;
+            Vector3 movement = Vector3.Lerp(startPosition, endPosition, t);
+            Debug.Log(movement - transform.position);
+            _characterController.Move(movement - transform.position);
             yield return new WaitForEndOfFrame();
         }
+        _characterController.Move(endPosition - transform.position);
     }
+    */
 
     public void Jump()
     {
+        /*
         if (transform.position.y <= 1.1)
         {
-            _rigidbody.AddForce(_entityStats.jumpForce * transform.up, ForceMode.Impulse);
-        }
+            _characterController.AddForce(_entityStats.jumpForce * transform.up, ForceMode.Impulse);
+        }*/
     }
 
     public void Roll()
     {
+        /*
         if (!roll)
         {
             roll = true;
             StartCoroutine(I_Roll());
-        }
+        }*/
     }
+    /*
 
     private IEnumerator I_Roll()
     {
@@ -112,5 +103,26 @@ public class Player_Movement : MonoBehaviour
         _collider.height = defHeight;
         _collider.center = defCenter;
         roll = false;
+    }*/
+    
+    
+    public void Hit()
+    {
+        /*
+        _entityStats.speed = (_entityStats.speed+_entityStats.defSpeed)/1.5f;
+        StartCoroutine(I_FlashPlayer());*/
+    }
+
+    private IEnumerator I_FlashPlayer()
+    {
+        Vector3 dposition = playerModel.transform.position;
+        
+        for (int i = 0; i < 5; i++)
+        {
+            playerModel.transform.Translate(Vector3.up * 1000);
+            yield return new WaitForSeconds(0.2f);
+            playerModel.transform.Translate(Vector3.up * -1000);
+            yield return new WaitForSeconds(0.2f);
+        }
     }
 }
