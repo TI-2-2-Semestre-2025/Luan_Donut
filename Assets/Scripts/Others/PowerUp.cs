c781e0cdb8fd6ee817fd589e11c969be8286f61b
Bernardo O. Horta <bernardo.o.horta@gmail.com>
2025-11-16 03:47:42 -0300

    Coin Magnet

--- Assets/Scripts/Others/PowerUp.cs
+++ Assets/Scripts/Others/PowerUp.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class PowerUp : MonoBehaviour
 {
     public int coinValue;
+    public int coinMagnetSeconds;
     
     public bool isHealth;
     public bool isCoin;
+    public bool isCoinMagnet;
     
     private Renderer rend;
     
@@ -24,10 +26,17 @@ public class PowerUp : MonoBehaviour
     
     private void OnTriggerEnter(Collider Trigger)
     {
+        if (Trigger.gameObject.CompareTag("CoinMagnet"))
+        {
+            if (isCoin) Debug.Log(1);
+            if (isCoin) Coin(Player: Game_Manager.Instance.Player.gameObject);
+            return;
+        }
         if (Trigger.gameObject.CompareTag("Player"))
         {
             if (isHealth) Health(Player: Trigger.gameObject);
             if (isCoin) Coin(Player: Trigger.gameObject);
+            if (isCoinMagnet) CoinMagnet(Player: Trigger.gameObject);
         }
     }
     
@@ -42,4 +51,23 @@ public class PowerUp : MonoBehaviour
         Player.GetComponent<Player_EntityStats>().PlayerCollectables.AddCoin(coinValue);
         Destroy(gameObject);
     }
-}
+
+    private void CoinMagnet(GameObject Player)
+    {
+        StartCoroutine(I_CoinMagnet(Player));
+    }
+
+    private IEnumerator I_CoinMagnet(GameObject Player)
+    {
+        Player_Collision PC = Player.GetComponent<Player_Collision>();
+        gameObject.GetComponentInChildren<MeshRenderer>().enabled = false;
+
+        PC.CoinMagnetCreate();
+        Game_Manager.Instance.UI_HUD.CoinMagnetManage(active: true);
+        yield return new WaitForSeconds(coinMagnetSeconds);
+        PC.CoinMagnetDestroy();
+        Game_Manager.Instance.UI_HUD.CoinMagnetManage(active: false);
+        
+        Destroy(gameObject);
+    }
+} 
